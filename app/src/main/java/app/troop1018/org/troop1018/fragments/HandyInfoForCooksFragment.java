de948ea9909aed5718c65ffe05029ac7fd0f5ed4
dridgeway2012 <52545621+dridgeway2012@users.noreply.github.com>
2019-08-25 19:43:33 -0500

    troop 90 app

---
+++ app/src/main/java/app/troop1018/org/troop1018/fragments/HandyInfoForCooksFragment.java
@@ -0,0 +1,25 @@
+package app.troop1018.org.troop1018.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.troop1018.org.troop1018.R;
+
+public class HandyInfoForCooksFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_handy_info_for_cooks, container, false);
+    }
+}
